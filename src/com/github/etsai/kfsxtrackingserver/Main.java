a1bc4e7004a19c1f613b9730c53bcbb207ef63fc
Eric Tsai <thegod0fde4th@gmail.com>
2012-06-12 02:07:33 -0700

    Setup netbeans project

---
+++ src/com/github/etsai/kfsxtrackingserver/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.github.etsai.kfsxtrackingserver;
+
+/**
+ * Main entry point for the tracking server
+ * @author etsai
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
