a7fa2620ea79d4d9fab9c462d58a8f22f75e9fa0
Eric Tsai <thegod0fde4th@gmail.com>
2012-06-12 20:38:57 -0700

    Added logger variable to log to file

---
+++ src/com/github/etsai/kfsxtrackingserver/Core.java
@@ -0,0 +1,18 @@
+package com.github.etsai.kfsxtrackingserver;
+
+
+import java.util.logging.Logger;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Central variables used by all classes.
+ * The objects must be thread safe
+ * @author etsai
+ */
+public class Core {
+    public static final Logger logger= Logger.getLogger("KFSXTrackingServer");
+}
--- src/com/github/etsai/kfsxtrackingserver/Main.java
+++ src/com/github/etsai/kfsxtrackingserver/Main.java
@@ -4,25 +4,27 @@
  */
 package com.github.etsai.kfsxtrackingserver;
 
+import static com.github.etsai.kfsxtrackingserver.Core.logger;
 import static com.github.etsai.kfsxtrackingserver.ServerProperties.UDP_PORT;
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Date;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.util.logging.*;
 
 /**
  * Main entry point for the tracking server
  * @author etsai
  */
 public class Main {
-
+    private static FileHandler logFileHandler;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Properties props;
-        UDPListener listener;
-        Thread udpTh;
         CommandLine clom= CommandLine.parse(args);
         
         try {
@@ -31,9 +33,43 @@ public class Main {
             Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             props= ServerProperties.getDefaults();
         }
-        listener= new UDPListener(Integer.valueOf(props.getProperty(UDP_PORT)));
-        udpTh= new Thread(listener);
+        
+        UDPListener listener= new UDPListener(Integer.valueOf(props.getProperty(UDP_PORT)));
+        Thread udpTh= new Thread(listener);
+        
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+                logFileHandler.close();
+            }
+        });
         
         udpTh.start();
+    }
+    
+    public static void initLogging() {
+        String filename;
+        String localHostAddress;
+        
+        try {
+            localHostAddress= InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException ex) {
+            logger.log(Level.SEVERE, "Error getting hostname of local host", ex);
+            localHostAddress= "unknown";
+        }
+        
+        filename= String.format("%s.%s.%tY%<tm%<td-%<tH%<tM%<tS.log", 
+            "udpstatstracker", localHostAddress, new Date());
+        try {
+            logFileHandler= new FileHandler(filename);
+            logFileHandler.setFormatter(new SimpleFormatter());
+        
+            for(Handler handler: logger.getHandlers()) {
+                logger.removeHandler(handler);
+            }
+            logger.addHandler(logFileHandler);
+        } catch (IOException|SecurityException ex) {
+            logger.log(Level.SEVERE, "Cannot write to file: "+filename, ex);
+        } 
     }
 }
