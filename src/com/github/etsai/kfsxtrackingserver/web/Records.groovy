f9ea0d459b2d327050ee783aaeb3ab145b1cc49f
etsai <thegod0fde4th@gmail.com>
2012-08-18 04:46:01 -0700

    Fixed bug with next/prev arrows messing up the start and end positions
    For example, given 73 records with 50 per page, going to page 2 would
    print out 24-73 instead of 51-73

--- src/com/github/etsai/kfsxtrackingserver/web/Records.groovy
+++ src/com/github/etsai/kfsxtrackingserver/web/Records.groovy
@@ -28,23 +28,26 @@ public class Records extends Page {
     
     public static final def KEY_PAGE= "page"
     public static final def KEY_ROWS= "rows"
-    public static final def DEFAULT_PAGE= 1
+    public static final def DEFAULT_PAGE= 0
     public static final def DEFAULT_ROWS= 50
     private def page, rows
     
     public def Records(def queries) {
-        page= queries[KEY_PAGE] == null ? DEFAULT_PAGE : [queries[KEY_PAGE].toInteger(), 1].max()
+        page= queries[KEY_PAGE] == null ? DEFAULT_PAGE : [queries[KEY_PAGE].toInteger(), DEFAULT_PAGE].max()
         rows= queries[KEY_ROWS] == null ? DEFAULT_ROWS : queries[KEY_ROWS].toInteger()
     }
     
     public String fillBody(def xmlBuilder) {
-        def start, end
         def allRecords= Common.statsData.getRecords().toArray()
+        def start= page * rows
+        def end= start + rows - 1
+        def numRecords= allRecords.size()
         
-        start= (page-1)* rows
-        end= start + rows - 1
-        if (start >= allRecords.size()) start= [0, allRecords.size() - rows].max()
-        if (end >= allRecords.size()) end= allRecords.size()-1
+        if (start >= numRecords) {
+            start= [0, numRecords - (numRecords % rows)].max()
+            page= (start / numRecords).toInteger() + 1
+        }
+        if (end >= numRecords) end= numRecords-1
         
         xmlBuilder.kfstatsx() {
             'records'(page: page, rows: rows) {
