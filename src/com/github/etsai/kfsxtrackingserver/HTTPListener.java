ef42f517dde1250bbf43aa5937a20000a87123e3
etsai <thegod0fde4th@gmail.com>
2012-11-04 02:22:21 -0800

    Removed unused import

--- src/com/github/etsai/kfsxtrackingserver/HTTPListener.java
+++ src/com/github/etsai/kfsxtrackingserver/HTTPListener.java
@@ -4,7 +4,6 @@ import static com.github.etsai.kfsxtrackingserver.Common.logger;
 import static com.github.etsai.kfsxtrackingserver.Common.pool;
 import com.github.etsai.kfsxtrackingserver.web.Page;
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
