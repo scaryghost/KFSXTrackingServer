ce04936aa75678b8b6c89aa413248124c91aea06
etsai <etsai2@gmail.com>
2013-04-18 23:07:21 -0700

    Removed the http folder from this project

--- http/groovy/ProfileHtml.groovy
+++
@@ -1,9 +0,0 @@
-import com.github.etsai.kfsxtrackingserver.DataReader
-import com.github.etsai.kfsxtrackingserver.web.Resource
-
-public class ProfileHtml implements Resource {
-    public String generatePage(DataReader reader, Map<String, String> queries) {
-        def nav= ["profile"].plus(reader.getAggregateCategories().collect{it.category}) << "sessions"
-        return WebCommon.generateHtml(nav, queries["steamid64"])
-    }
-}
