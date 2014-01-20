6034e339dc26cc58b5f911e99f3c5c6f6a68a305
etsai <etsai2@gmail.com>
2014-01-20 06:20:41 -0800

    Added checks to ensure query names are unique and a valid query name is executed

--- src/com/github/etsai/kfsxtrackingserver/ReaderWrapper.java
+++ src/com/github/etsai/kfsxtrackingserver/ReaderWrapper.java
@@ -23,16 +23,20 @@ public class ReaderWrapper {
         annotatedMethods= new HashMap<>();
         for(Method it: readerClass.getMethods()) {
             if (it.isAnnotationPresent(Query.class)) {
-                annotatedMethods.put(((Query)it.getAnnotation(Query.class)).name(), it);
+                String name= ((Query)it.getAnnotation(Query.class)).name();
+                if (annotatedMethods.containsKey(name)) {
+                    throw new RuntimeException(String.format("Duplicate query name found: %s", name));
+                }
+                annotatedMethods.put(name, it);
             }
         }
     }
     
     public Object executeQuery(String name, Object...args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        if (annotatedMethods.containsKey(name)) {
-            return annotatedMethods.get(name).invoke(readerObj, args);
+        if (!annotatedMethods.containsKey(name)) {
+            throw new RuntimeException(String.format("Query name '%s' not found", name));
         }
-        return null;
+        return annotatedMethods.get(name).invoke(readerObj, args);
     }
     public Object getReaderObject() {
         return readerObj;
